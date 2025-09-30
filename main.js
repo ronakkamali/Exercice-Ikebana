dbf47cb6db02e18abb4813877e669e8009a80927
r.kamali <r.kamali@interface3.be>
2025-09-30 11:26:53 +0200

    correction

--- main.js
+++ main.js
@@ -20,6 +20,8 @@ document.addEventListener("DOMContentLoaded", () => {
     const nav = document.querySelector("nav");
 
     hamburger.addEventListener("click", () => {
+        
+        
         nav.classList.toggle("active");
     });
 });
